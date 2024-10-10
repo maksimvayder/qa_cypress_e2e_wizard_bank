d43b4d5506417e94e3e3e99e9359422641eb9122
Maksim <teseygrecia@gmail.com>
2024-10-10 18:13:31 +0300

    Solution

--- cypress/e2e/hermioneAccount.spec.js
+++ cypress/e2e/hermioneAccount.spec.js
@@ -1,3 +1,4 @@
+import { data } from '../support/commands.js';
 /// <reference types='cypress' />
 
 describe('Bank app', () => {
@@ -5,7 +6,47 @@ describe('Bank app', () => {
     cy.visit('/');
   });
 
-  it('should provide the ability to work with Hermione\'s bank account', () => {
+  it('should enable interaction with Hermione\'s bank account', () => {
+    cy.contains('.btn', 'Customer Login').click();
+    cy.get('[name="userSelect"]').select(data.user);
+    cy.contains('.btn', 'Login').click();
 
+    cy.contains('[ng-hide="noAccount"]', 'Account Number')
+      .contains('strong', data.accountNumber)
+      .should('be.visible');
+    cy.contains('[ng-hide="noAccount"]', 'Balance')
+      .contains('strong', data.balance)
+      .should('be.visible');
+    cy.contains('.ng-binding', data.currency).should('be.visible');
+
+    cy.get('[ng-click="deposit()"]').click();
+    cy.get('[placeholder="amount"]').type(data.depositValue);
+    cy.contains('[type="submit"]', 'Deposit').click();
+
+    cy.get('[ng-show="message"]').should('contain', 'Deposit Successful');
+    cy.contains('[ng-hide="noAccount"]', 'Balance').should('be.visible');
+
+    cy.get('[ng-click="withdrawl()"]').click();
+    cy.contains('[type="submit"]', 'Withdraw').should('be.visible');
+    cy.get('[placeholder="amount"]').type(data.withdrawlValue);
+    cy.contains('[type="submit"]', 'Withdraw').click();
+
+    cy.get('[ng-show="message"]').should('contain', 'Transaction successful');
+    cy.contains('[ng-hide="noAccount"]', 'Balance')
+      .contains('strong', data.balance)
+      .should('be.visible');
+
+    cy.get('[ng-click="transactions()"]').click();
+    cy.get('td:nth-child(3)').eq(-2).should('contain', 'Credit');
+    cy.get('td:nth-child(3)').last().should('contain', 'Debit');
+
+    cy.get('[ng-click="back()"]').click();
+    cy.get('[id="accountSelect"]').select('1002');
+
+    cy.get('[ng-click="transactions()"]').click();
+    cy.get('tr#anchor1').should('not.exist');
+
+    cy.get('[ng-click="byebye()"]').click();
+    cy.get('#userSelect').should('exist');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,14 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const data = {
+  user: 'Hermoine Granger',
+  accountNumber: 1001,
+  balance: 5096,
+  currency: 'Dollar',
+  depositValue: 1000,
+  withdrawlValue: 1000,
+  randomTransaction: Math.floor(Math.random() * 8)
+};
+
+module.exports = { data };
